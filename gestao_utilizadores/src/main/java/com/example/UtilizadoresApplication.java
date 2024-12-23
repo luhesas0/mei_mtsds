f6742b7df53e9f247d638ff3e515ed2e3bf27714
Celia Paiva <8220954@estg.ipp.pt>
2024-12-23 15:10:31 +0000

    SpringBootApplication run Utilizadores

---
+++ gestao_utilizadores/src/main/java/com/example/UtilizadoresApplication.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ */
+@SpringBootApplication
+public class UtilizadoresApplication {
+    public static void main(String[]args){
+        SpringApplication.run(UtilizadoresApplication.class,args);
+    }
+}
