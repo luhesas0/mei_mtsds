74a4127e85509a9401e3ca64537a4132978a9ed0
Celia Paiva <8220954@estg.ipp.pt>
2025-01-09 00:59:31 +0000

    Iniciado Contexto de autorização de tarefas

---
+++ gestao_utilizadores/src/main/java/org/example/enums/TipoAtor.java
@@ -0,0 +1,26 @@
+package org.example.utils;
+
+/**
+ * Enumeração para representar subtipos de funcionários no sistema.
+ */
+public enum TipoAtor {
+    FUNCIONARIO_ENTREGADOR("Funcionario Entregador"),
+    FUNCIONARIO_STOCK("Funcionario de Stock"),
+    FUNCIONARIO_MENU("Funcionário de Menu"),
+    FUNCIONARIO_REPOSITORIO("Funcionário de Repositório");
+
+    private final String descricao; //Descrição amigável para o tipo de ator do sistema
+
+    TipoAtor(String descricao){
+        this.descricao = descricao;
+    }
+
+    /**
+     * Obtém a descrição do tipo de ator.
+     *
+     * @return Descrição do tipo de ator.
+     */
+    public String getDescricao(){
+        return descricao;
+    }
+}
---
+++ gestao_utilizadores/src/main/java/org/example/enums/UtilizadorRule.java
@@ -0,0 +1,25 @@
+package org.example.models;
+
+/**
+ * Enumeração que define os papéis dos utilizadores no sistema.
+ */
+public enum UtilizadorRule {
+    ADMIN("Administrador"),
+    FUNCIONARIO("Funcionario"),
+    CLIENTE("Cliente");
+
+    private final String role;
+
+    UtilizadorRule(String role){
+        this.role = role;
+    }
+
+    /**
+     * Obtém o papel associado ao enum.
+     *
+     * @return String representando o papel.
+     */
+    public String getRole(){
+        return role;
+    }
+}
