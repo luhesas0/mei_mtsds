622b97b901e3fe1ee9864ca116d849586ffb1367
JFFS800 <8240047@estg.ipp.pt>
2025-01-22 21:54:13 +0000

    Corrigir o Stock.dto.java, com o niveis minimos

--- verifica_stock/src/main/java/com/example/dto/StockDTO.java
+++ verifica_stock/src/main/java/com/example/dto/StockDTO.java
@@ -2,19 +2,13 @@ package com.example.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
-@ToString
-
 public class StockDTO {
-
     private Long id;
     private String name;
-    private int quantity;
+    private Integer quantity;
     private double price;
-
+    private Integer nivelMinimo;
 }
